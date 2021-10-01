1e03c5d46c71354c245bb21d3ffc93477935a3a9
issiy <allmylifewriter@gmail.com>
2021-10-02 07:28:50 +0900

    cookie参考用

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ require('dotenv').config();
 const bodyParser = require('body-parser')
 const path = require('path')
 
+// Cookieを読み込むために必要な記述
+const cookieParser = require('cookie-parser')
+
 const app = express()
 // エンコーディングするための処理
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const ejs = require('ejs') //追加
+app.use(cookieParser())
 
 const port = 3000
 
 const mysql = require('mysql');
+// 静的画像ファイル表示
+app.use(express.static('static'));
+
 
 
 // TODO　環境に応じて接続先を変更する処理
@@ -28,6 +35,10 @@ db.connect((err) => {
     if(err) throw err
     console.log("Mysqlに接続しました。")
 })
+
+// app.get('/', (req, res) => {
+// 	res.send('Hello Express!!');
+// })
 
 app.get('/', (req, res) => {
   const sql = "select * from users";
@@ -46,6 +57,17 @@ app.post('/', (req, res) => {
     res.send("登録完了")
 })
 })
+
+app.use((req, res, next) => {
+	if (req.session.username) {
+	  next();
+	} else {
+	  res.redirect('/login');
+	}
+  });
+
+app.get('/create', (req, res) => 
+	res.sendFile(path.join(__dirname, 'html/form.html')))
 
 
 app.get('/users', (req, res) => {
@@ -79,6 +101,26 @@ app.get('/delete/:id', (req, res) => {
 	if (err) throw err;
 	res.redirect('/')
 	});
+});
+
+//=========================
+// cookie編
+//=========================
+
+app.get('/cookie', (req, res) => {
+	const answer = req.cookies.answer
+	res.render('cookie', { answer });
+});
+
+app.post('/cookie', (req, res) => {
+	console.log("req", req.body)
+	let answer = ""
+	if(req.body.question === "買う"){
+		answer = "買う"
+	}else if(req.body.question === "買わない"){
+		answer = "買わない"
+	}
+	res.cookie("answer", answer).redirect('/cookie')
 });
 
 //　ユーザ作成
